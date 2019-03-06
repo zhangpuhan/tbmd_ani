241092199c93f239d8fcd72fabfcea964ca79a2e
Puhan Zhang <pz4ee@virginia.edu>
2019-03-06 12:20:55 -0500

    030620191220

--- new_aev.py
+++ new_aev.py
@@ -3,7 +3,8 @@ import os
 import torch
 import neurochem
 import aev
-import pyanitool as pya
+import natsort
+import time
 
 torch.set_default_tensor_type('torch.DoubleTensor')
 
@@ -37,5 +38,45 @@ print(coordinate.unsqueeze(0))
 
 test_data = aev_computer.forward((consts.species_to_tensor('HHHHHHHHHH').unsqueeze(0),
                                   coordinate.unsqueeze(0).double()))[1]
-pd.DataFrame(torch.squeeze(test_data, 0).cpu().numpy()).to_csv("test_data.csv", index=None, header=None)
+test_data = torch.cat((test_data.squeeze(0), force), dim=1)
+pd.DataFrame(test_data.cpu().numpy()).to_csv("test_data.csv", index=None, header=None)
 print(test_data)
+
+
+def locate_files(file_path):
+    filenames_a = []
+    for _, __, files in os.walk(file_path):
+        for filename in files:
+            if filename.endswith(".csv"):
+                filenames_a.append(file_path + "/" + filename)
+    filenames_a = natsort.natsorted(filenames_a)
+
+    return filenames_a
+
+
+filenames = locate_files("testdata")
+
+
+def process_files(filenames_a):
+
+    print(str(len(filenames_a)) + " files need to be processed")
+    for i in range(801):
+        print("********************************************")
+        print("Data file " + filenames_a[i] + " is being processing:")
+        print(aev_computer)
+
+        data_a = pd.read_csv(filenames_a[i], header=None)
+
+        coordinate_tensor_a = torch.tensor(data_a.values)
+
+        force_a = coordinate_tensor_a[:, 3:]
+        coordinate_a = coordinate_tensor_a[0:, :3]
+
+        test_data_a = aev_computer.forward((consts.species_to_tensor('HHHHHHHHHH').unsqueeze(0),
+                                            coordinate_a.unsqueeze(0).double()))[1]
+        test_data_a = torch.cat((test_data_a.squeeze(0), force_a), dim=1)
+        file_string = "train/" + "ga" + str(i) + ".csv"
+        pd.DataFrame(test_data_a.cpu().numpy()).to_csv(file_string, index=None, header=None)
+
+
+process_files(filenames)
