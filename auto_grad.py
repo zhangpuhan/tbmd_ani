b0e9e77460b000e768ca050a1ffa42cec846b8bf
Puhan Zhang <pz4ee@virginia.edu>
2019-03-09 23:02:42 -0500

    030920192302

--- auto_grad.py
+++ auto_grad.py
@@ -1,12 +1,56 @@
 import torch
+import torch.nn.functional as F
 
 torch.manual_seed(7)
 train_data = torch.rand((10000, 3), requires_grad=True)
 add_noise = torch.randn((10000, 1))/500.0
 print(train_data)
 # with torch.no_grad():
-train_y = torch.reshape(torch.sum(train_data**2, dim=1)**0.5, (-1, 1)) + add_noise
+train_y = torch.reshape(torch.sum(train_data**2, dim=1)**0.5, (-1, 1))
 
 v = torch.reshape(torch.ones(10000), (-1, 1))
 train_y.backward(v)
-print(train_data.grad)
+
+y = train_data.grad
+with torch.no_grad():
+    train_y = train_y.clone()
+print(train_y)
+print(train_y.mean())
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature):
+        super(Net, self).__init__()
+        self.input = torch.nn.Linear(n_feature, 64)
+        self.hidden = torch.nn.Linear(64, 32)
+        self.output = torch.nn.Linear(32, 1)
+
+    def forward(self, x):
+        x = F.relu(self.input(x))
+        x = F.relu(self.hidden(x))
+        x = self.output(x)
+        return x
+
+
+x_train = torch.rand((10000, 3), requires_grad=True)
+
+net = Net(n_feature=3)
+
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+loss_func = torch.nn.MSELoss(reduction="sum")
+
+for t in range(50):
+    for j in range(0, 10000, 10):
+        train_temp = x_train[j: j + 10]
+        prediction = net(train_temp)
+
+        loss = loss_func(prediction, train_y[j: j + 10])
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # print(prediction, train_y[j: j + 10])
+        if j == 5000:
+            print(loss.data.numpy())
+
+print(net(x_train))
---
+++ grad_model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn.functional as F
+
+# torch.manual_seed(1)    # reproducible
+
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
+y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
+
+# torch can only train on Variable, so convert them to Variable
+# The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
+# x, y = Variable(x), Variable(y)
+
+# plt.scatter(x.data.numpy(), y.data.numpy())
+# plt.show()
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_output):
+        super(Net, self).__init__()
+        self.input = torch.nn.Linear(n_feature, 3)   # hidden layer
+        self.hidden_1 = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation function for hidden layer
+        x = self.predict(x)             # linear output
+        return x
+
+
+net = Net(n_feature=1, n_hidden=10, n_output=1)     # define the network
+print(net)  # net architecture
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
+loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+
+for t in range(200):
+    prediction = net(x)     # input x and predict based on x
+
+    loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
+
+    optimizer.zero_grad()   # clear gradients for next train
+    loss.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
