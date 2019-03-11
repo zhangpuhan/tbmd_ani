5e81574ffffdbece0d3fe2ab41e9f4f68918090c
Puhan Zhang <pz4ee@virginia.edu>
2019-03-10 21:16:18 -0400

    031020192116

--- auto_grad.py
+++ auto_grad.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn.functional as F
+import torch.utils.data as Data
 
 torch.manual_seed(7)
 train_data = torch.rand((10000, 3), requires_grad=True)
-add_noise = torch.randn((10000, 1))/500.0
+add_noise = torch.randn((10000, 1))/5000.0
 print(train_data)
 # with torch.no_grad():
-train_y = torch.reshape(torch.sum(train_data**2, dim=1)**0.5, (-1, 1))
+train_y = torch.reshape(torch.sum(train_data**2, dim=1)**0.5, (-1, 1)) + add_noise
 
 v = torch.reshape(torch.ones(10000), (-1, 1))
 train_y.backward(v)
@@ -21,13 +22,15 @@ print(train_y.mean())
 class Net(torch.nn.Module):
     def __init__(self, n_feature):
         super(Net, self).__init__()
-        self.input = torch.nn.Linear(n_feature, 64)
-        self.hidden = torch.nn.Linear(64, 32)
-        self.output = torch.nn.Linear(32, 1)
+        self.input = torch.nn.Linear(n_feature, 512)
+        self.hidden_1 = torch.nn.Linear(512, 256)
+        self.hidden_2 = torch.nn.Linear(256, 128)
+        self.output = torch.nn.Linear(128, 1)
 
     def forward(self, x):
         x = F.relu(self.input(x))
-        x = F.relu(self.hidden(x))
+        x = F.relu(self.hidden_1(x))
+        x = F.relu(self.hidden_2(x))
         x = self.output(x)
         return x
 
@@ -36,21 +39,32 @@ x_train = torch.rand((10000, 3), requires_grad=True)
 
 net = Net(n_feature=3)
 
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-loss_func = torch.nn.MSELoss(reduction="sum")
+optimizer = torch.optim.SGD(net.parameters(), lr=0.1)
+loss_func = torch.nn.MSELoss(reduction="mean")
+
+torch_dataset = Data.TensorDataset(x_train, train_y)
+loader = Data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=100,
+    shuffle=True
+)
+
+
+v = torch.reshape(torch.ones(100), (-1, 1))
 
 for t in range(50):
-    for j in range(0, 10000, 10):
-        train_temp = x_train[j: j + 10]
-        prediction = net(train_temp)
+    for step, (batch_x, batch_y) in enumerate(loader):  # for each training step.
+        prediction = net(batch_x)
 
-        loss = loss_func(prediction, train_y[j: j + 10])
+        loss = loss_func(prediction, batch_y)
+        print('Epoch: ', t, '| Step: ', step, '| loss: ', loss.data.numpy())
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        # print(prediction, train_y[j: j + 10])
-        if j == 5000:
-            print(loss.data.numpy())
 
-print(net(x_train))
+x_train_2 = torch.rand((10000, 3))
+y_train_2 = net.forward(x_train_2)
+print(y_train_2)
+
+print(((y_train_2 - train_y)**2.0).sum()/10000.0)
